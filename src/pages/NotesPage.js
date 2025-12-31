3ddf7cf63235efd8a9e5c4c0cce044c1a533266d
SuttonKitty <sut.fritz04@gmail.com>
2025-12-31 16:57:58 -0500

    Update NotesPage.js

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -81,7 +81,7 @@ const images = [
 const NotesPage = () => {
   return (
     <div className="flex flex-col md:p-10 w-full items-start text-base md:text-5xl gap-[1.25rem] md:gap-10">
-      <div className="text-sm font-[monospace] text-justify md:text-6xl">
+      <div className="text-sm font-[monospace] md:font-[Inter] text-justify md:text-6xl">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
       <NoteCard
